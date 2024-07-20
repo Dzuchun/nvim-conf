994d4135149d6fddeebc89bed5642615a6ba06c7
Dzuchun <Infernodambik@gmail.com>
2024-07-20 12:53:48 +0300

    Added linting support
    Only for js/ts for now. I plan on adding new language lints as I start
    working on projects

---
+++ nvim/lua/dzu/plugins/lint.lua
@@ -0,0 +1,30 @@
+function config()
+    local is_ok, lint = pcall(require, 'lint')
+    if not is_ok then
+        vim.notify("Failed to setup lint");
+        return;
+    end
+
+    local web_linters = { 'biomejs' }
+    lint.linters_by_ft = {
+        javascript = web_linters,
+        javascriptreact = web_linters,
+        typescript = web_linters,
+        typescriptreact = web_linters,
+        json = web_linters,
+    }
+
+    -- autolint on save
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+        pattern = { "*.js", "*.ts", "*.jsx", ".tsx", "*.json" },
+        callback = function(ev)
+            lint.try_lint()
+        end,
+    })
+end
+
+return {
+    "mfussenegger/nvim-lint",
+    config = config,
+    lazy=false,
+}
