06da042af57bce10b7f81bb822497d8150db60ee
Dzuchun <Infernodambik@gmail.com>
2024-08-23 22:02:37 +0300

    Strict clippy as rust linter

--- nvim/lua/dzu/lsp/rust_analyzer.lua
+++ nvim/lua/dzu/lsp/rust_analyzer.lua
@@ -70,6 +70,21 @@ return {
                     enable = true -- let's try that :idk:
                 }
             },
+            checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = {
+                    '--', '-W', 'clippy::all', '-W', 'clippy::pedantic',
+                    '-A', 'clippy::used-underscore-binding',
+                    '-A', 'clippy::doc_markdown',
+                    '-A', 'clippy::needless_pass_by_value',
+                    '-A', 'clippy::must_use_candidate',
+                    '-A', 'clippy::return_self_not_must_use',
+                    '-A', 'clippy::missing_errors_doc',
+                    '-A', 'clippy::single_match',
+                    '-A', 'clippy::uninlined_format_args'
+                }
+            }
         }
     }
 }
