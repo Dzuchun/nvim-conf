a8509be64a5f7d28ed2da09e2238e1e1d8a2e57c
Dzuchun <Infernodambik@gmail.com>
2024-08-28 14:26:10 +0300

    Updated 'macro'
    Now contains utility functions to detect mathzones. Probably should move
    them elsewhere

--- nvim/lua/dzu/macro.lua
+++ nvim/lua/dzu/macro.lua
@@ -1,6 +1,8 @@
+local M = {}
+
 -- add toggle for white space helpers
 local whitespace_helpers = true
-function toggle_whitespace_helpers()
+function M.toggle_whitespace_helpers()
     if whitespace_helpers then
         whitespace_helpers = false
         vim.opt.list = false
@@ -12,8 +14,6 @@ function toggle_whitespace_helpers()
     end
 end
 
-toggle_whitespace_helpers()
-
 -- limiting line must be disable in readonly windows
 vim.api.nvim_create_autocmd("BufEnter", {
     callback = function(event_data)
@@ -23,6 +23,69 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
 })
 
-return {
-    toggle_whitespace_helpers = toggle_whitespace_helpers
+
+-- CRED: https://github.com/nvim-treesitter/nvim-treesitter/issues/1184#issuecomment-1079844699
+local has_treesitter, ts = pcall(require, 'vim.treesitter')
+local _, query = pcall(require, 'vim.treesitter.query')
+
+local MATH_NODES = {
+    displayed_equation = true,
+    inline_formula = true,
+    math_environment = true,
 }
+
+local COMMENT = {
+    ['comment'] = true,
+    ['line_comment'] = true,
+    ['block_comment'] = true,
+    ['comment_environment'] = true,
+}
+
+local function get_node_at_cursor()
+    local buf = vim.api.nvim_get_current_buf()
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    row = row - 1
+    col = col - 1
+
+    local ok, parser = pcall(ts.get_parser, buf, 'latex')
+    if not ok or not parser then return end
+
+    local root_tree = parser:parse()[1]
+    local root = root_tree and root_tree:root()
+
+    if not root then
+        return
+    end
+
+    return root:named_descendant_for_range(row, col, row, col)
+end
+
+function M.in_comment()
+    if has_treesitter then
+        local node = get_node_at_cursor()
+        while node do
+            if COMMENT[node:type()] then
+                return true
+            end
+            node = node:parent()
+        end
+        return false
+    end
+end
+
+function M.in_mathzone()
+    if has_treesitter then
+        local node = get_node_at_cursor()
+        while node do
+            if node:type() == 'text_mode' then
+                return false
+            elseif MATH_NODES[node:type()] then
+                return true
+            end
+            node = node:parent()
+        end
+        return false
+    end
+end
+
+return M
