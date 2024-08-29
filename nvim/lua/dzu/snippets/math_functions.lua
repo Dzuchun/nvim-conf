5c4211872353f4a5f720ccb0e67610145841c787
Dzuchun <Infernodambik@gmail.com>
2024-08-29 15:31:40 +0300

    Math snippets for sinh- and sinc-like functions

--- nvim/lua/dzu/snippets/math_functions.lua
+++ nvim/lua/dzu/snippets/math_functions.lua
@@ -27,12 +27,24 @@ local paren_content = macro.suffix_paren_content;
 local last_word = macro.last_word;
 local get_visual = macro.get_visual
 
-local function func_snip(func_name)
+local function func_snip(func_name, priority)
+    if priority == nil then
+        priority = 10
+    end
     return s({
         trig = func_name,
         condition = mathzone,
         snippetType = "autosnippet",
+        priority = priority,
     }, fmt([[\text{{{}}}\left({}\right)]], { t(func_name), i(1) }), {})
+end
+
+local function func_snip_add(func_name, add)
+    return s({
+        trig = "\\text{" .. func_name .. "}\\left(" .. add .. add,
+        condition = mathzone,
+        snippetType = "autosnippet",
+    }, fmt([[\text{{{}}}\left({}]], { t(func_name .. add), i(1) }), {})
 end
 
 return {
@@ -42,4 +54,15 @@ return {
     func_snip("tan"),
     func_snip("cot"),
     func_snip("exp"),
+    func_snip("sec"),
+    func_snip("csc"),
+    func_snip("arcsin", 20),
+    func_snip("arccos", 20),
+    func_snip("arctan", 20),
+    func_snip("arccot", 20),
+    func_snip_add("sin", "c"),
+    func_snip_add("sin", "h"),
+    func_snip_add("cos", "h"),
+    func_snip_add("tan", "h"),
+    func_snip_add("cot", "h"),
 }
