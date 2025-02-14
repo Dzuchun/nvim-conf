184428721c682071c5c864cae116b902f444c6a2
Dzuchun <Infernodambik@gmail.com>
2025-02-14 06:23:35 +0200

    Some ftplugin options
    most likely, this allows auto line breaks in Markdown, but forbids
    them in Rust

---
+++ nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,17 @@
+vim.opt_local.formatoptions = {
+    o = true,
+    ["/"] = true,
+
+    ["1"] = true,
+
+    w = true,
+    n = true,
+    q = true,
+    j = true,
+    p = true,
+    r = true,
+    l = true,
+    c = false,
+    t = false,
+    a = false,
+}
