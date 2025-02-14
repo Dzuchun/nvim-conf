9bac09125e9e2f8b5370fbab8078307c2b65f210
Dzuchun <Infernodambik@gmail.com>
2025-02-14 11:50:24 +0200

    Added other linters to `mason_lint`

--- nvim/lua/dzu/plugins/mason_lint.lua
+++ nvim/lua/dzu/plugins/mason_lint.lua
@@ -6,7 +6,13 @@ local function config()
     end
 
     local opts = {
-        ensure_installed = { 'biome' },
+        ensure_installed = {
+            "black",    -- python formatter
+            "biome",    -- web formater
+            "fourmolu", -- haskell formatter
+            "nixfmt",   -- nix formatter
+            "clang_format",
+        },
     }
 
     mason_nvim_lint.setup(opts);
